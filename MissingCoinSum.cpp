1bd1dee464f995a40f78e69d16bf436da4f9d80f
PSOI <saichaita@gmail.com>
2022-02-04 01:31:32 +0530

    04 Feb, 2022

--- MissingCoinSum.cpp
+++ MissingCoinSum.cpp
@@ -12,34 +12,24 @@ int main()
     
     int n;
     cin>>n;
-
-
-    //1 2 2 7 9
-    vector<int> coins(n);
-    for (int i = 0; i < n; i++) cin>>coins[i];
-    sort(coins.begin(), coins.end());
-    // cout<<"# ";
-    unordered_map<long long,long long> wallet;
-    int flag;
-    long long req_coin=1;
-    vector<long long> curr;
-
-    for(auto x:coins) {
-        // cout<<"* ";
-        curr.clear();
-        flag=0;
-        while(req_coin-x==0 || wallet[req_coin-x]==1) {
-            // cout<<req_coin<<" ";
-            curr.push_back(req_coin);
-            flag=1;
-            req_coin++;
-        }
-        for(auto y:curr) wallet[y]++;
-        wallet[x]=1;
-        if(flag==0) break;
+    
+    vector<int> denomination(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>denomination[i];
     }
-
-    cout<<req_coin;
+    sort(denomination.begin(), denomination.end());
+    long long req=1;
+    
+    for(auto x:denomination) {
+        if(x<=req) {
+            req += x;
+        }
+        else {
+            break;
+        }
+    }
+    cout<<req;
 
     return 0;
 }
